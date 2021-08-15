b00425c641ca5285002348de1ca7881911efb24c
ferhat <ferhat-uzun@outlook.com>
2021-08-15 16:34:24 +0300

    First

---
+++ app/src/main/java/com/example/landmarkbook/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.landmarkbook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.landmarkbook.databinding.ActivityDetailsBinding
+import com.example.landmarkbook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var landmarkList: ArrayList<Landmark>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        landmarkList = ArrayList<Landmark>()
+
+        val pisa = Landmark("Pisa","Italy",R.drawable.pisa)
+        val colosseum = Landmark("Colosseum","Italy",R.drawable.colosseum)
+        val eiffel = Landmark("Eiffel","France",R.drawable.eiffel)
+        val londonBridge = Landmark("London Bridge","UK",R.drawable.londonbridge)
+
+        landmarkList.add(pisa)
+        landmarkList.add(colosseum)
+        landmarkList.add(eiffel)
+        landmarkList.add(londonBridge)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter=Adapter(landmarkList)
+        binding.recyclerView.adapter=adapter
+
+//        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,landmarkList.map { landmark -> landmark.name })
+//
+//        binding.listView.adapter=adapter
+//
+//        binding.listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+//            val intent = Intent(MainActivity@this,DetailsActivity::class.java)
+//            intent.putExtra("landmark",landmarkList.get(i))
+//            startActivity(intent)
+//        }
+
+    }
+}
